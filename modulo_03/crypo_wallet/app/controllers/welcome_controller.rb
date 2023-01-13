5dd865dfbe4a36ef4ec40fc99244cc3e0f646801
Diogo <diogoslva0133@gmail.com>
2023-01-13 12:08:37 -0300

    add: cookies and session

--- modulo_03/crypo_wallet/app/controllers/welcome_controller.rb
+++ modulo_03/crypo_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails - Diogo[COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails - Diogo[SESSION]"
     @meu_nome = params[:nome]
     @curso = params[:curso]
   end
