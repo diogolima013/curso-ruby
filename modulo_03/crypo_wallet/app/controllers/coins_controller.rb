0f8782b28798c251bec83800eef2d42ee7112796
Diogo <diogoslva0133@gmail.com>
2023-01-13 08:47:08 -0300

    add: Defaulting select to MVC

--- modulo_03/crypo_wallet/app/controllers/coins_controller.rb
+++ modulo_03/crypo_wallet/app/controllers/coins_controller.rb
@@ -2,7 +2,9 @@ class CoinsController < ApplicationController
   layout "adm"
   
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: %i[new create edit update]
 
+  
   # GET /coins or /coins.json
   def index
     @coins = Coin.all
@@ -60,6 +62,11 @@ class CoinsController < ApplicationController
   end
 
   private
+
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id) 
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
