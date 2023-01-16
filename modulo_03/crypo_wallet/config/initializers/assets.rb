c6b6b5b33fcc63db0bfca3e8ac485439ed10cedc
Diogo <diogoslva0133@gmail.com>
2023-01-16 09:16:02 -0300

    add: isolate assets to controller

--- modulo_03/crypo_wallet/config/initializers/assets.rb
+++ modulo_03/crypo_wallet/config/initializers/assets.rb
@@ -11,4 +11,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( welcome.css coins.css mining_types.css scaffolds.css )
+
+Rails.application.config.assets.precompile += %w( cable.js welcome.js coins.js mining_types.js scaffolds.js )
